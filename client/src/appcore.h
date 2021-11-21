4fc54b0d3d3318238b32a20312bacb254a4d367b
Justin Shannon <zla.jshannon@gmail.com>
2021-11-20 16:50:53 -0800

    Remove unused code

--- client/src/appcore.h
+++ client/src/appcore.h
@@ -1,35 +1,6 @@
 #pragma once
 
-#include <QObject>
-#include "src/common/versioncheck.h"
-
-class QQmlApplicationEngine;
-class QQmlEngine;
-class QJSEngine;
-
 namespace xpilot
 {
     int Main(int argc, char* argv[]);
-
-    class AppCore : public QObject
-    {
-        Q_OBJECT
-
-    public:
-        AppCore(QQmlEngine* qmlEngine);
-        virtual ~AppCore() {}
-
-        static QObject* appConfigInstance(QQmlEngine* engine, QJSEngine* scriptEngine);
-
-    signals:
-        void serverListDownloaded(int count);
-        void serverListDownloadError();
-        void newVersionAvailable(QString version, QString downloadUrl);
-        void noUpdatesAvailable();
-
-    private:
-        QQmlApplicationEngine* engine;
-        void DownloadServerList();
-        void PerformVersionCheck();
-    };
 }
