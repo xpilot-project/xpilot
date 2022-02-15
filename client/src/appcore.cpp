dc9f9285bfb70f05d102d7d6763a63f1521b4cd2
Justin Shannon <zla.jshannon@gmail.com>
2022-02-15 07:37:52 -0800

    Enable high DPI scaling again

--- client/src/appcore.cpp
+++ client/src/appcore.cpp
@@ -53,6 +53,7 @@ int xpilot::Main(int argc, char* argv[])
     auto sentryClose = qScopeGuard([]{ sentry_close(); });
 
     QCoreApplication::setAttribute(Qt::AA_UseOpenGLES);
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QCoreApplication::setApplicationName("xPilot");
     QCoreApplication::setApplicationVersion(xpilot::BuildConfig::getVersionString());
     QCoreApplication::setOrganizationName("Justin Shannon");
