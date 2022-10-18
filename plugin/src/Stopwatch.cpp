dd095b9b123c83fe4c6f850479fd68b0c38ce23d
Justin Shannon <zla.jshannon@gmail.com>
2022-10-17 21:10:38 -0600

    Fix define

--- plugin/src/Stopwatch.cpp
+++ plugin/src/Stopwatch.cpp
@@ -110,7 +110,9 @@ namespace xpilot {
 	}
 
 #ifdef _WIN32
+#ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
+#endif // !WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 #include <stdint.h> // portable: uint64_t   MSVC: __int64 
 
