86e6df3b76cfb5b49b3f4cd8742d880167b4820f
Justin Shannon <zla.jshannon@gmail.com>
2022-12-03 20:44:26 -0700

    Add AppImage tools instead of downloading

--- scripts/appimage.sh
+++ scripts/appimage.sh
@@ -5,13 +5,10 @@ APPIMAGE_PATH="xPilot.AppDir"
 rm -rf $APPIMAGE_PATH
 mkdir -p $APPIMAGE_PATH
 
-wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage
-chmod +x appimagetool-x86_64.AppImage
-
-wget https://github.com/AppImage/AppImageKit/releases/download/continuous/AppRun-x86_64
+chmod +x ../scripts/appimagetool-x86_64.AppImage
 mkdir -p ${APPIMAGE_PATH}/usr/lib
 mkdir -p ${APPIMAGE_PATH}/usr/bin
-mv AppRun-x86_64 ${APPIMAGE_PATH}/AppRun
+mv ../scripts/AppRun-x86_64 ${APPIMAGE_PATH}/AppRun
 chmod +x ${APPIMAGE_PATH}/AppRun
 
 # Copy icons
@@ -29,5 +26,5 @@ cp xpilot.png ${APPIMAGE_PATH}/
 cp build/xpilot ${APPIMAGE_PATH}/usr/bin/
 chmod +x ${APPIMAGE_PATH}/usr/bin/xpilot
 
-./appimagetool-x86_64.AppImage ${APPIMAGE_PATH}
+../scripts/appimagetool-x86_64.AppImage ${APPIMAGE_PATH}
 mkdir ../dist && mv xPilot*.AppImage* ../dist/xPilot.AppImage
