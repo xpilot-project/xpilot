546a1201dc057b4ffe57e84c22b10ab50d07b739
Justin Shannon <zla.jshannon@gmail.com>
2023-01-20 18:08:50 -0700

    Add missing license headers

--- client/src/network/nearbyatc.h
+++ client/src/network/nearbyatc.h
@@ -1,3 +1,21 @@
+/*
+ * xPilot: X-Plane pilot client for VATSIM
+ * Copyright (C) 2019-2023 Justin Shannon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+*/
+
 #ifndef NEARBYATC_H
 #define NEARBYATC_H
 
@@ -16,39 +34,5 @@ public:
     int sim_frequency;
 };
 Q_DECLARE_METATYPE(NearbyAtc)
-
-//struct Station {
-//    Q_GADGET
-//    Q_PROPERTY(QString callsign MEMBER callsign)
-//    Q_PROPERTY(QString realname MEMBER realname)
-//    Q_PROPERTY(QString frequency MEMBER frequency)
-//    Q_PROPERTY(int sim_frequency MEMBER sim_frequency)
-//public:
-//    QString callsign;
-//    QString realname;
-//    QString frequency;
-//    int sim_frequency;
-//};
-//Q_DECLARE_METATYPE(Station)
-
-//struct NearbyAtc {
-//    Q_GADGET
-//    Q_PROPERTY(QList<Station> center MEMBER center)
-//    Q_PROPERTY(QList<Station> approach MEMBER approach)
-//    Q_PROPERTY(QList<Station> tower MEMBER tower)
-//    Q_PROPERTY(QList<Station> ground MEMBER ground)
-//    Q_PROPERTY(QList<Station> delivery MEMBER delivery)
-//    Q_PROPERTY(QList<Station> atis MEMBER atis)
-//    Q_PROPERTY(QList<Station> observers MEMBER observers)
-//public:
-//    QList<Station> center;
-//    QList<Station> approach;
-//    QList<Station> tower;
-//    QList<Station> ground;
-//    QList<Station> delivery;
-//    QList<Station> atis;
-//    QList<Station> observers;
-//};
-//Q_DECLARE_METATYPE(NearbyAtc)
 
 #endif // NEARBYATC_H
